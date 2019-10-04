cb27bba1ba9cdc3f206a4ee15c2f176efdb4f3aa
skliarenkopa <56111971+skliarenkopa@users.noreply.github.com>
2019-10-04 13:03:39 +0300

    Working example

--- cpp/iwu_task_3.cpp
+++ cpp/iwu_task_3.cpp
@@ -17,7 +17,15 @@ std::vector<std::string> MakeUniqueFilenames(std::vector<std::string> names)
 {
 	std::vector<std::string> result;
 
-	
+	for (const auto& name : names) {
+		std::string unq_name = name;
+		int suffix_num = 1;
+		while (std::find(result.begin(), result.end(), unq_name) != result.end()) {
+			unq_name = name + std::string("(") + std::to_string(suffix_num) + std::string(")");
+			suffix_num++;
+		}
+		result.push_back(unq_name);
+	}
 
 	return result;
 }
@@ -25,7 +33,7 @@ std::vector<std::string> MakeUniqueFilenames(std::vector<std::string> names)
 void PrintSequences(std::vector<std::string> sequences)
 {
 	std::cout << '[';
-	for (auto& seq : sequences) {
+	for (const auto& seq : sequences) {
 		std::cout << '"' << seq << '"';
 		if (&seq != &sequences.back())
 			std::cout << ", ";
@@ -39,7 +47,8 @@ int main()
 	std::vector<std::string> filenames = { "photo", "photo", "doc", "photo(1)", "photo" }; // ["photo", "photo(1)", "doc", "photo(1)(1)", "photo(2)"]
 	//std::vector<std::string> filenames = { "name", "name", "name(1)", "name(3)", "name(2)", "name(2)" }; // ["name", "name(1)", "name(1)(1)", "name(3)", "name(2)", "name(2)(1)"]
 	//std::vector<std::string> filenames = { "a", "b", "cd", "b ", "a(3)" }; // ["a", "b", "cd", "b ", "a(3)"]
-	
+	//std::vector<std::string> filenames = { "name", "name", "name", "name", "name", "name" }; // ["name", "name(1)", "name(2)", "name(3)", "name(4)", "name(5)"]
+
 	std::vector<std::string> result = MakeUniqueFilenames(filenames);
 	PrintSequences(result);
 }
